540edd6e7ca81d4cba0b2feba955e4fa190bed4c
nikunjan <nikunjangc@gmail.com>
2017-11-12 14:50:00 -0500

    first commit

---
+++ app/src/main/java/nyc/c4q/sharedpreference/SecondActivity.java
@@ -0,0 +1,19 @@
+package nyc.c4q.sharedpreference;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class SecondActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+        TextView textView =(TextView) findViewById(R.id.session_message_textview) ;
+       Intent intent= getIntent();
+       String user = intent.getStringExtra("currentUser");
+       textView.setText("you are currently signed as "+user);
+    }
+}
